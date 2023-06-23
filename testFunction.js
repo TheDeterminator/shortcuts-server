be6013e9341e34effa6bc13ffe271a8f517233f2
Alec Jordan <alecjordan@Alecs-Air.lan>
2023-06-23 00:59:29 -0700

    update add files to database need null checks

--- testFunction.js
+++ testFunction.js
@@ -39,31 +39,21 @@ const removeAt = (dateString) => {
 const getDateAndNotes = (dateString) => {
     let [date, notes] = dateString.split('(');
     notes = notes ? notes.replace(')', '') : ""
-    // return { dateTimestamp: convertDateToTimeStamp(date.trim()), notes: notes.trim() }
     return { dateTimestamp: date.trim(), notes: notes.trim() }
 };
 
-// const convertDateToTimeStamp = (dateString) => {
-//     return Date.parse(dateString);
-// }
-
-const getDates = async () => {
-    const fileData = await fs.readFile('goToSleepTimes.txt', 'utf8')
+const getDates = async (filePath) => {
+    const fileData = await fs.readFile(filePath, 'utf8')
     const dateArray = fileData.split('\n').slice(1) // remove the first line
 
     return dateArray.map(dateLine => {
-        // console.log({dateLine})
         const dateWithAtRemoved = removeAt(dateLine) // TODO: rename to something like "clean date" maybe
         const { dateTimestamp, notes } = getDateAndNotes(dateWithAtRemoved)
-        // console.log(dateTimestamp, notes)
         return { dateTimestamp, notes }
     })
 }
 
-// getDates()
-
 const buildSleepDataTable = async () => {
-    // TODO: Implement this to rebuild that table whenever you want to start over or change things
     // -- Define the ENUM type for event
     const enumsCreationQuery = `CREATE TYPE sleep_event_type AS ENUM ('go_to_sleep', 'wake_up', 'nap');`
 
@@ -96,50 +86,41 @@ const dropSleepDataTable = async () => {
     }
 }
 
+const insertInDatabase = async (eventType, filePath) => {
+    let dateData = await getDates(filePath)
 
-const insertInDataBase = async () => { // TODO do this, but with wake up times and nap times and fap times
+    try {
+        for (const dd of dateData) {
+            let query = 'INSERT INTO sleep_data (event_timestamp, event_type, notes) VALUES ($1, $2, $3) RETURNING *';
+            let values = [dd.dateTimestamp, eventType, dd.notes];
+            let res = await client.query(query, values);
+            console.log(res.rows[0]);
+        }
+
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+const cleanDatabase = async () => {
     try {
         await dropSleepDataTable()
         await buildSleepDataTable()
     } catch (err) {
         console.error(err);
     }
-
-    let dateData = await getDates()
-
-    try {
-        for (const dd of dateData) {
-            let query = 'INSERT INTO sleep_data (event_timestamp, event_type, notes) VALUES ($1, $2, $3) RETURNING *';
-            let values = [dd.dateTimestamp, 'go_to_sleep', dd.notes];
-            let res = await client.query(query, values);
-            console.log(res.rows[0]);
-        }
-
-        // array.prototype.forEach causes weird behavior with async/await
-        // dateData.forEach(async dd => {
-
-        // let query = 'INSERT INTO sleep_data (event_timestamp, event_type, notes) VALUES ($1, $2, $3) RETURNING *';
-        // let values = [dd.dateTimestamp, 'go_to_sleep', dd.notes];
-
-        // let res = await client.query(query, values);
-        // console.log(res.rows[0]);
-        // })
-
-
-        // Another way to do the above. But have to be cognizant of unhandled promise rejections
-        // Or using Promise.all with Array.prototype.map:
-        // await Promise.all(dateData.map(async (dd) => {
-        //     let query = 'INSERT INTO sleep_data (event_timestamp, event_type, notes) VALUES ($1, $2, $3) RETURNING *';
-        //     let values = [dd.dateTimestamp, 'go_to_sleep', dd.notes];
-        //     let res = await client.query(query, values);
-        //     console.log(res.rows[0]);
-        // }));
-    } catch (err) {
-        console.error(err);
-    }
-    finally {
-        client.end();
-    }
 }
 
-insertInDataBase()
+// TODO: Add null checks to all this stuff
+// (async () => await cleanDatabase())().catch(err => console.error(err))
+
+(async () => {
+    // await buildSleepDataTable()
+    await Promise.all([
+        insertInDatabase('wake_up', './wakeUpTime.txt')
+        , insertInDatabase('go_to_sleep', './goToSleepTimes.txt')
+    ])
+
+    await client.end();
+})().catch(err => console.error(err))
