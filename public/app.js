4fa3df851ea96b40d8c1f3d61251b8936d90c73a
Alec Jordan <alecjordan@MacBook-Pro-37.lan>
2023-06-30 13:59:59 -0700

    utc -> local on tables, remove buttons change table styling

--- public/app.js
+++ public/app.js
@@ -1,20 +1,20 @@
-document.getElementById('getScreenWidth').addEventListener('click', function() {
-    var screenWidth = window.innerWidth;
-    console.log('Screen width: ' + screenWidth);
+// document.getElementById('getScreenWidth').addEventListener('click', function() {
+//     var screenWidth = window.innerWidth;
+//     console.log('Screen width: ' + screenWidth);
     
-    // Send screenWidth to your server here.
-    // This depends on your server setup. An example using fetch:
-    fetch('screen-width', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ width: screenWidth }),
-    })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-});
+//     // Send screenWidth to your server here.
+//     // This depends on your server setup. An example using fetch:
+//     fetch('screen-width', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ width: screenWidth }),
+//     })
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//     .catch((error) => {
+//       console.error('Error:', error);
+//     });
+// });
 
